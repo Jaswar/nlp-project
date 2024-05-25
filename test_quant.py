ae7eed87aca9e2fbe3b595ea3fad466a199c8314
Jan Warchocki <janwarchocki@gmail.com>
2024-05-25 22:38:56 +0200

    Added the dummy classifier

--- test_quant.py
+++ test_quant.py
@@ -1,6 +1,6 @@
 import argparse
 import json
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
 
 def main(args):
@@ -26,20 +26,34 @@ def main(args):
     print()
 
     print('Accuracy per category:')
-    for category, preds in preds_per_category.items():
+    sorted_categories = sorted(preds_per_category.keys())
+    for category in sorted_categories:
+        preds = preds_per_category[category]
         y_pred, y_true = zip(*preds)
         acc = accuracy_score(y_true, y_pred)
-        print(f'{category}: {round(acc * 100, 2)}%')
-    print()
+        f1 = f1_score(y_true, y_pred, average='weighted')
+        if not args.latex:
+            print(f'F1 score for {category}: {round(f1 * 100, 2)}%')
+            print(f'Accuracy for {category}: {round(acc * 100, 2)}%')
+        else:
+            print(f'{round(f1 * 100, 2)} & {round(acc * 100, 2)} & ', end='')
+    if not args.latex:
+        print()
 
     # This should match the accuracy from the main training script
-    print(f'Overall accuracy: {round(accuracy_score(ground_truth, predictions) * 100, 2)}%')
-
+    overall_accuracy = accuracy_score(ground_truth, predictions)
+    overall_f1 = f1_score(ground_truth, predictions, average='weighted')
+    if not args.latex:
+        print(f"Overall F1 score: %")
+        print(f'Overall accuracy: %')
+    else:
+        print(f'{round(overall_f1 * 100, 2)} & {round(overall_accuracy * 100, 2)} \\\\')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--claims_path', type=str, help='Path to the .json file containing the claims')
     parser.add_argument('--pred_path', type=str, help='Path to the .csv file containing the predictions')
     parser.add_argument('--gt_path', type=str, help='Path to the .csv file containing the ground truth')
+    parser.add_argument('--latex', type=bool, default=False, help='Whether to output the results in a latex format')
     args = parser.parse_args()
     main(args)
